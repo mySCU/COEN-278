a64109e47bcdda87ae901bf9c32f20ba8b5f9916
Amblizer <me@amblizer.xyz>
2019-05-23 01:13:23 -0700

    hw1 separated as submodule

--- hw/hw1/q6.rb
+++
@@ -1,7 +0,0 @@
-def puts_advisor(file_name)
-    File.readlines(file_name).each { |line|   # open file, default read only, close automatically
-        if (line =~ /^ADVISOR.*/) != nil
-            puts "#{line}"
-        end
-    }    
-end
